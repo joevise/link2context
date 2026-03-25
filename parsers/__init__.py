8e872fdce05392218bd07ecbc8aba7ce26c6c55d
joevise <elttilz5@gmail.com>
2026-03-25 16:54:02 +0800

    feat: link2context - 修复 Playwright 抓取和图片下载超时问题
    - base_parser.py: 修复 clean_html 噪音词匹配 bug
    - dynamic_parser.py: 修复无效参数 wait_after_interactions
    - media_handler.py: 图片下载超时 8s + ThreadPoolExecutor 并发

---
+++ parsers/__init__.py
@@ -0,0 +1,5 @@
+from .wechat_parser import WeChatParser
+from .static_parser import StaticParser
+from .dynamic_parser import DynamicParser
+
+__all__ = ["WeChatParser", "StaticParser", "DynamicParser"]
